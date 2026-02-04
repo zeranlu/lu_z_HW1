2718b5de8cadb28fa5d4cef9bce296b60965863a
Zeran Lu <zeranlu0326@gmail.com>
2026-02-04 17:15:59 -0500

    Update main.js
    In this commit, correct database linking was solved and translated. Basic content loading logic was written

--- js/main.js
+++ js/main.js
@@ -5,11 +5,35 @@ const baseURL = `https://swapi.info/api/films/`;
     
     function getMovies() {
         fetch(`${baseURL}`)
-        .then(response => response.json())
-        .then(function(response) {
-            console.log(response.episode_id)
+        .then((res) => res.json())
+        .then(function(res) {
+
+            console.log(res);
+
+            const movies = res;
+            const ul = document.createElement("ul");
+
+            movies.forEach((movie, index) => {
+                console.log(`Film ${index + 1}: ${movie.title}: ${movie.episode_id}`)
+
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.textContent = movie.title;
+                li.appendChild(a);
+                li.appendChild(a);
+
+                ul.appendChild(li);
+            })
+
+            movieBox.appendChild(ul);
+
+            
+
+
+
+            
         })
-        .catch()
+        .catch((error) => console.error(error))
     }
     
 
