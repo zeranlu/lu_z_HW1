ebc09d7d58aa0cda3aa77101149d6a807161c06c
Zeran Lu <zeranlu0326@gmail.com>
2026-02-06 17:53:19 -0500

    added scrollTo
    In this commit, added scrollTo animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { fetchRequest } from "./modules/fetch.js";
 import { navbar_showhide } from "./modules/hamburger.js";
 import { greensockAnim } from "./modules/greensock.js";
+import { scrollTo } from "./modules/scrollTo.js";
 
+scrollTo();
 greensockAnim();
 fetchRequest();
 navbar_showhide();
